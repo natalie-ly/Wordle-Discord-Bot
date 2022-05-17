f017a43046fa743611d3cd721187a2b680a6bfbe
Natalie Ly <n6ly@uwaterloo.ca>
2022-05-17 18:54:33 -0400

    Create search command

---
+++ commands/info/search.js
@@ -0,0 +1,20 @@
+const gis = require('g-i-s')
+module.exports = {
+    name: "search",
+    category: "info",
+    //array of permissions contain permissions that member is required to have to execute this command
+    permissions: [],
+    devOnly: false,
+    async execute(message, search) {
+
+        gis(search, function logResults(error, results) {
+          if (error) {
+            console.log(error);
+            message.reply('Something went wrong')
+          }
+          else {
+            message.reply(results[Math.floor(Math.random()*results.length)].url)
+          }
+        })
+    }
+}
---
+++ index.js
@@ -0,0 +1,50 @@
+const Discord = require("discord.js")
+
+const search = require("./commands/info/search")
+
+// allows .env variable to be loaded into global variable that can be accessed anywhere in the code process.env
+require("dotenv").config() 
+
+const client = new Discord.Client({
+    intents: 32767
+})
+
+const prefix = "-"
+
+const welcomeId = "352235792967401472"
+
+client.on("guildMemberAdd", async (member) => { 
+    const image = await createImage(member)
+    member.guild.channels.cache.get(welcomeId).send({
+        content: `<@${member.id}> Welcome!`,
+        files: [image]
+    })
+})
+
+client.once("ready", () => {
+    console.log(`Bot now online in ${client.guilds.cache.size} servers.`)
+})
+
+client.on("messageCreate", (message) => { 
+    if (message.author.bot) return;
+ 
+    const args = message.content.slice(prefix.length).trim().split(/ +/g)
+    const firstArg = args.shift()
+    if (!firstArg) return;
+    const command = firstArg.toLowerCase()
+
+    if (command === 'hello') {
+        message.reply("Hi")
+    }
+
+    if (command === 'search') {
+        search.execute(message, args.join(' '))
+    }
+
+    if (command === 'ping') {
+        message.reply("Pong")
+    }
+
+})
+
+client.login(process.env.TOKEN)
